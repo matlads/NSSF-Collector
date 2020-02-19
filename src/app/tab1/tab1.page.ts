027e7399570c8530f5a17405d034ff21f6c6875f
Martin Atukunda <matlads@gmail.com>
2020-02-19 11:27:35 +0300

    don't allow searching without a username

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { EmployersService } from '../services/employers.service';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-tab1',
@@ -9,14 +10,31 @@ import { EmployersService } from '../services/employers.service';
 export class Tab1Page {
   searchTerm: string = '';
   data: any = [];
-  
-  constructor(private employersService: EmployersService) {
+  username: string;
+  password: string;
+
+  constructor(private employersService: EmployersService, private storage: Storage) {
+    this.getUsername();
+    this.getPassword();
   }
 
   setFilteredItems(){
-    if (this.searchTerm.length >= 3) {
-      this.data = this.employersService.filterItems(this.searchTerm);
+    if ( this.username ) {
+      if (this.searchTerm.length >= 3) {
+        this.data = this.employersService.filterItems(this.searchTerm);
+      }
     }
   }
 
+  getUsername() {
+		this.storage.get('username').then(val => {
+      this.username = val
+    });
+  }
+
+  getPassword() {
+    this.storage.get('password').then(val => {
+      this.password = val
+    });
+  }
 }
